8df99cb57a2e0e537e0f26bcd06845a2a292e211
kubyaddi <kubyaddi@users.noreply.github.com>
2019-07-09 10:25:40 +0900

    add smpinfo.js

---
+++ smpinfo/smpinfo.js
@@ -0,0 +1,33 @@
+const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
+let running = false;
+let sensval = {"accx" : null, "accy" : null, "accz" : null};
+
+async function reset() {
+  document.querySelector("#result").innerHTML = "";
+}
+
+async function toggle() {
+  if(running) {
+    running = false;
+  }
+  else {
+    running = true;
+    return new Promise((resolve, result) => {
+      writesensors();
+    });
+  }
+}
+
+async function writesensors() {
+  while(running) {
+    document.querySelector("#result").innerHTML += sensval["accx"] + "," + sensval["accy"] + "," + sensval["accz"];
+    document.querySelector("#result").innerHTML += "\n";
+    await sleep(100);
+  }
+}
+
+window.addEventListener("devicemotion", event = {
+  sensval["accx"] = event.acceleration.x;
+  sensval["accy"] = event.acceleration.y;
+  sensval["accz"] = event.acceleration.z;
+});
